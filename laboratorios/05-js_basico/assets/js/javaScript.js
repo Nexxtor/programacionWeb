357703f3e962398c712cb1c73a0dbdf796234b07
DouglasHdezT <00004516@uca.edu.sv>
2018-09-18 21:45:09 -0600

    Introduccion javascript

--- laboratorios/05-js_basico/assets/js/javaScript.js
+++ laboratorios/05-js_basico/assets/js/javaScript.js
@@ -1,3 +1,7 @@
+var front_image =  document.getElementById("down-button");
+
+front_image.addEventListener("click", scrollToStart);
+
 function scrollToStart(){
     var startPosition =  document.getElementById("title-content").offsetTop;
     window.scroll({
