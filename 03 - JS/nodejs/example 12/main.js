27732606e4b85ded243c4a27efe07153443a67a0
Nestor Aldana <nextor@MacBook-Pro-de-Mauricio-2.local>
2018-10-31 10:38:55 -0600

    Add Console.log to show a objURL value

--- 03 - JS/nodejs/example 12/main.js
+++ 03 - JS/nodejs/example 12/main.js
@@ -5,6 +5,7 @@ const url = require('url');
 http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type': 'text/html'});
     var objURl = url.parse(req.url,true);
+    console.log(objURl);
     res.write(JSON.stringify(objURl));
     res.end();
 }).listen(8080);
