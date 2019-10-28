762f418a3cc644ddcb7a339104a2fd74d20b45a7
Néstor Aldana <nestor.aldana1@gmail.com>
2019-10-28 15:26:48 -0600

    Delete naldan

--- naldana/routes/dashboard.js
+++
@@ -1,9 +0,0 @@
-const express = require('express'),
-    router = express.Router(),
-    dashboardController =  require('../controllers/DashboardController');
-
-router.get('/', dashboardController.home);
-router.post('/menu', dashboardController.new);
-router.get('/menu/:id', dashboardController.getOneMenu);
-
-module.exports = router;
